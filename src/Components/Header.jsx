7c57081c4216bf10d92a17574b9dbf905ef38f6d
Fortune Nnah <Fortunennah747@gmail.com>
2026-04-02 18:00:41 +0100

    FIx: fixed header and created hero.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
+      <div className="logo">
+        <h1>AKautomobile</h1>
+      </div>
         <nav className='navLinks'>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">Contact</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
         </nav>
     </div>
   )
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="herosection">
+        <div className="herotxt">
+            <h1>Your Journey <br /> Starts Here.</h1>
+            <p>Discover the thrill of driving with our premium vehicles.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
